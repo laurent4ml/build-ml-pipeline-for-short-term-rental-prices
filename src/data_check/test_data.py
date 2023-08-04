68d6077f53e80e329345a9b7c2867581d6bb3323
Laurent <laurent@lanightlife.com>
2023-08-03 23:42:50 -0700

    adding data check step with test_result file

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -44,22 +44,31 @@ def test_proper_boundaries(data: pd.DataFrame):
     """
     Test proper longitude and latitude boundaries for properties in and around NYC
     """
-    idx = data['longitude'].between(-74.25, -73.50) & data['latitude'].between(40.5, 41.2)
+    idx = (data['longitude'].between(-74.25, -73.50)
+        & data['latitude'].between(40.5, 41.2))
 
     assert np.sum(~idx) == 0
 
 
-def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_threshold: float):
+def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame,
+                                kl_threshold: float):
     """
-    Apply a threshold on the KL divergence to detect if the distribution of the new data is
-    significantly different than that of the reference dataset
+    Apply a threshold on the KL divergence to detect if the distribution of the new data
+    is significantly different than that of the reference dataset
     """
     dist1 = data['neighbourhood_group'].value_counts().sort_index()
     dist2 = ref_data['neighbourhood_group'].value_counts().sort_index()
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
+def test_row_count(data):
+    '''
+    checks that the size of the dataset is reasonable (not too small, not too large)
+    '''
+    assert 15000 < data.shape[0] < 1000000
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_price_range(data, min_price, max_price):
+    '''
+    checks that the price column has values between min price and max price
+    '''
+    assert data['price'].between(min_price, max_price).all()
